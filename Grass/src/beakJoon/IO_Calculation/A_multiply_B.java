5b2895099d3275e262ae17bc8fcbf7fb1e462592
sunHwa <inhwa377@gmail.com>
2021-03-17 17:53:51 +0900

    백준

---
+++ Grass/src/beakJoon/IO_Calculation/A_multiply_B.java
@@ -0,0 +1,20 @@
+package beakJoon.IO_Calculation;
+
+import java.util.Scanner;
+
+public class A_multiply_B {
+
+	public static void main(String[] args) {
+		int A;
+		int B;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		System.out.println(A*B);
+
+	}
+
+}
