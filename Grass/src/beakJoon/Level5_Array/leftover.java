356d864d3c9fb015a4803d65476646b11f4e87d2
autum <autum@DESKTOP-5982GDG>
2021-04-13 03:45:18 +0900

    .

--- Grass/src/beakJoon/Level5_Array/leftover.java
+++ Grass/src/beakJoon/Level5_Array/leftover.java
@@ -1,34 +1,21 @@
 package beakJoon.Level5_Array;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class leftover {
 
 	public static void main(String[] args) {
-		int[] num = new int[10];
-		int cnt = 0;
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer> hs = new HashSet<Integer>();
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		try {
-			
-			for(int i=0; i<10; i++) {
-				
-			}
-			
-		
-			bw.newLine();
-			bw.flush();
-			bw.close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
+		for(int i=0; i<10; i++) {
+			hs.add(sc.nextInt() % 42);
 		}
+		
+		sc.close();
+		System.out.print(hs.size());
+		
 	}
 
 }
--- Grass/src/beakJoon/Main.java
+++ Grass/src/beakJoon/Main.java
@@ -1,40 +1,20 @@
 package beakJoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Main {
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer> hs = new HashSet<Integer>();
 		
-		int[] arr = new int[10];
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		try {
-
-			int mul = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
-			
-			String str = String.valueOf(mul);
-			
-			for(int i=0; i<str.length(); i++) {
-				arr[(str.charAt(i) - 48)]++;
-			}
-			
-			for(int j : arr) {
-				bw.write(String.valueOf(j));
-				bw.newLine();
-				bw.flush();
-			}
-			
-			bw.close();
-			
-		}catch (Exception e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
+		for(int i=0; i<10; i++) {
+			hs.add(sc.nextInt() % 42);
 		}
+		
+		sc.close();
+		System.out.print(hs.size());
+		
 	}
 }
