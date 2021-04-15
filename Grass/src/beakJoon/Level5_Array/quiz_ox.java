95f711daacd594df62130bc20d0c6d81c910ceb5
inhwa377 <inhwa377@gmail.com>
2021-04-15 23:52:26 +0900

    .

---
+++ Grass/src/beakJoon/Level5_Array/quiz_ox.java
@@ -0,0 +1,13 @@
+package beakJoon.Level5_Array;
+
+import java.util.Scanner;
+
+public class quiz_ox {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
